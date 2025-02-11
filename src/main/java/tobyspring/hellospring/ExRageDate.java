c10098b3a3f67dafb8f27f7f822dd3a4d99b1bd1
zunstar <shimkijun@naver.com>
2025-02-11 20:49:59 +0900

    PaymentService 개발-3

---
+++ src/main/java/tobyspring/hellospring/ExRageDate.java
@@ -0,0 +1,10 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRageDate(String result , Map<String, BigDecimal> rates) {
+}
