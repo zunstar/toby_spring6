6b259081896586c0245243b7d5be59994c64b002
zunstar <shimkijun@naver.com>
2025-02-12 12:48:59 +0900

    관심사의 분리

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -7,28 +7,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        // 환율 가져오기
-        //https://open.er-api.com/v6/latest/USD
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-        ObjectMapper mapper = new ObjectMapper();
-        ExRageDate data = mapper.readValue(response, ExRageDate.class);
-        BigDecimal exRate = data.rates().get("KRW");
+        BigDecimal exRate = getExRate(currency);
 
-        // 금액 계산
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // 유효 시간 계산
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(
@@ -39,6 +26,19 @@ public class PaymentService {
                 convertedAmount,
                 validUntil
         );
+    }
+
+    private BigDecimal getExRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRageDate data = mapper.readValue(response, ExRageDate.class);
+        BigDecimal exRate = data.rates().get("KRW");
+        return exRate;
     }
 
     public static void main(String[] args) throws IOException {
