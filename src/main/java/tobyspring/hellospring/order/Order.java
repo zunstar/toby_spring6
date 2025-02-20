404c4267aebb7d88e99d9fc29efe473850d30200
zunstar <shimkijun@naver.com>
2025-02-21 00:01:47 +0900

    기술에 독립적인 애플리케이션 서비스

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -4,14 +4,11 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
+
 public class Order {
 
-    @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
