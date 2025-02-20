d78e835698751f986f9aa11b090f1c2ede7cb898
zunstar <shimkijun@naver.com>
2025-02-20 23:41:36 +0900

    애플리케이션 서비스 도입

--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -47,8 +47,4 @@ public class DataConfig {
         return new JpaTransactionManager(emf);
     }
 
-    @Bean
-    public OrderRepository orderRepository(){
-        return new OrderRepository();
-    }
 }
---
+++ src/main/java/tobyspring/hellospring/OrderClient.java
@@ -0,0 +1,19 @@
+package tobyspring.hellospring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import tobyspring.hellospring.order.Order;
+import tobyspring.hellospring.order.OrderService;
+
+import java.math.BigDecimal;
+
+public class OrderClient {
+    public static void main(String[] args){
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(OrderConfig.class);
+        OrderService service = beanFactory.getBean(OrderService.class);
+
+        Order order = service.createOrder("0100",BigDecimal.TEN);
+        System.out.println(order);
+    }
+}
